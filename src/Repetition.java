469fed3a392975d8e6fa0ceecfcfc68b4cdfd9fb
SaraEngsted <sara.andreasen@live.dk>
2021-11-04 18:19:12 +0100

    almost

--- src/Repetition.java
+++ src/Repetition.java
@@ -8,16 +8,20 @@ import java.util.Scanner;
 
 public class Repetition {
 
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
 
         //Exercise 1
+        System.out.println("-------- Exercise 1 -------");
+        //
+
 
         //Exercise 2 - - exemplify the concept inheritance
 
-        System.out.println("------Exercise 2------");
+        System.out.println();
+        System.out.println("------ Exercise 2 ------");
 
         Exercise2Cars stationCar = new Exercise2Cars("BMW", "red", 220, 50);
         System.out.println(stationCar);
@@ -28,7 +32,8 @@ public class Repetition {
 
 
         //Exercise 3 -
-        System.out.println("------Exercise 3-------");
+        System.out.println();
+        System.out.println("------ Exercise 3 -------");
 
         ArrayList<String> words = new ArrayList<String>();
 
@@ -45,6 +50,7 @@ public class Repetition {
         System.out.println("This is how you get the amount of words there is in the file = " + words.size());
 
         //Exercise 4 -
+        System.out.println();
         System.out.println("------Exercise 4-------");
 
         //How many birth and deaths compared to the current population:
@@ -72,6 +78,30 @@ public class Repetition {
         System.out.println("the population in 5 years will be " + populationIn5Years);
 
 
+        //Exercise 5
+        System.out.println();
+        System.out.println("------- Exercise 5 -------");
+        //First the program generates random numbers from 0 to 10.
+        //Then it prints out the random numbers.
+        //The method below makes sure that there are only 10 numbers to choose from.
+
+
+        //Exercise 6
+        System.out.println();
+        System.out.println("-------- Exercise 6 ------");
+
+        ArrayList<Integer> numbers1to100 = new ArrayList<Integer>();
+
+        for (int i = 0; i < 100; i++) {
+            numbers1to100.add(i);
+        }
+
+        System.out.println(numbers1to100);
+        System.out.println("Please choose a number to remove the multiples of that number from the list");
+        int chosenNumber = scanner.nextInt();
+
+        numbers1to100.removeIf(integer -> (integer % chosenNumber == 0 && integer != chosenNumber));
+        System.out.println(numbers1to100);
 
 
 
